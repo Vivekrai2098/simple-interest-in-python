62583d339afa76030cb1e10f793e25b5cb000ae4
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-03-12 13:33:05 +0530

    update

---
+++ The Simple Interst in Python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#         THE SIMPLE INTEREST IN PYTHON
+
+p=eval(input("enter your principal amount:-")) 
+r=eval(input("enter your  rate amount:-"))
+t=eval(input("enter your time amount:-"))
+interest=p*r*t/100
+print("your interest value is:=",interest)
+total=interest+p
+print("your total paybal amount is:-",total)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
